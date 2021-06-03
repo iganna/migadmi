71d8aeacbdf75a92a9dd5f883300d5d7385120bc
Анна Иголкина <anna@Annas-MacBook-Pro.local>
2021-06-03 11:52:49 +0200

    migadmi testing

--- migadmi_opt.py
+++ migadmi_opt.py
@@ -15,18 +15,18 @@ from tree_cov import eval_mx, eval_vect, is_branch, is_weight, is_alpha, \
     create_admixture_alpha, define_used_weights, get_cov, get_dist
 
 
-def likelihood(params, wnd, d_mx, variables_d, alpha):
+def likelihood(params, dist_curr, d_mx, variables_d, alpha):
     """
     Computes the likelihood value
     :param params: values of parameters
-    :param wnd: observed matrix of differences
+    :param dist_curr: observed matrix of differences
     :param d_mx: symbolic matrix of differences between populations
     :param variables_d: symbols for parameters
     :param alpha: parameter for the Dirichlet prior
     :return:
     """
 
-    n_pop = wnd.shape[0]
+    n_pop = dist_curr.shape[0]
     d_values = eval_mx(d_mx, variables_d, params).tolist()
 
     var_tol = 10 ** -10
@@ -36,8 +36,9 @@ def likelihood(params, wnd, d_mx, variables_d, alpha):
         variance = d_values[i][j]
         if np.abs(variance) < var_tol:
             variance = var_tol
-        ll_value += norm.logpdf(x=wnd.iloc[i, j],
-                                loc=0, scale=float(variance ** (1/2)))
+        ll_value += norm.logpdf(x=float(dist_curr.iloc[i, j]),
+                                loc=0, scale=float(variance) ** (1/2))
+
 
     # # Ridge/Lasso
     # for i, v in enumerate(variables_d):
@@ -62,21 +63,27 @@ def likelihood(params, wnd, d_mx, variables_d, alpha):
     return -ll_value
 
 
-def param_estim(d_mx, variables_d, weight_sets, var_mix_all, wnd, alpha, opt_method='SLSQP'):
+def param_estim(d_mx, variables_d, weight_sets, dist_curr, alpha, opt_method='SLSQP'):
     """
 
-    :param d_mx:
-    :param variables_d:
-    :param weight_sets:
-    :param var_mix_all:
-    :param wnd:
+    :param d_mx: parametrized matrix with distances between leaves
+    :param variables_d: names of variables to optimize
+    :param weight_sets: to set constraits that all wight should sum up to 1
+        weight_sets is a list of sublists. Each sublist contains names of variables
+        that should sum up to 1
+    :param dist_curr: distance matrix with real values
+    :param alpha: parameter for the Dirichlet prior (=1 if no prior)
+    :param opt_method: optimisation method
     :return:
     """
 
+    # Boundaries for variables
     bnds = [(0, float('inf')) if is_branch(v) else (0, 1)
             for v in variables_d]
-    bnds = [(0.1, 1) if is_alpha(v) else bnds[i] for i, v in enumerate(variables_d)]
+    # bnds = [(0.1, 1) if is_alpha(v) else bnds[i] for i, v in enumerate(variables_d)]
+    bnds = [(0, 1) if is_alpha(v) else bnds[i] for i, v in enumerate(variables_d)]
 
+    # Constraints for variables
     cons_w = []
     for i in range(len(weight_sets)):
         if len(weight_sets[i]) == 0:
@@ -88,10 +95,11 @@ def param_estim(d_mx, variables_d, weight_sets, var_mix_all, wnd, alpha, opt_met
 
     linear_constraint = optimize.LinearConstraint(A, lbnd, upbnd)
 
-    obj_func = partial(likelihood, wnd=wnd, d_mx=d_mx, variables_d=variables_d, alpha=alpha)
+    obj_func = partial(likelihood, dist_curr=dist_curr, d_mx=d_mx, variables_d=variables_d, alpha=alpha)
 
     max_fun = 1000
-    for i in [2, 4, 6, 8]:
+    # for i in [1, 2, 4, 6, 8]:
+    for i in [1]:
         x0 = [i + 1 if is_branch(v) else 0.5
               for v in variables_d]
         if len(weight_sets) == 0:
@@ -105,28 +113,23 @@ def param_estim(d_mx, variables_d, weight_sets, var_mix_all, wnd, alpha, opt_met
 
     res = max_res
 
-    variances_all = [eval_vect(var_mix, variables_d, res.x) for var_mix in var_mix_all]
-    variances_val = [[float(v / sum(variances)) for v in variances] for variances in variances_all]
-
-    return res.x, [item for sublist in variances_val for item in sublist]
+    return res.x
 
 
-def opt_wnd(pop_names, popset_init,
-            pop_admix,
-            pop_sources,
-            admixture_steps,
+
+
+def popdisp(pop_names,
+            popset_init,
             variables_init,
-            path_data_in,
-            path_data_out,
-            pref_wnd_init='wnd_',
-            pref_out_res='res_',
-            pref_out_var='var_',
+            pop_admix,
+            admixture_steps,
+            dist_matrix,
+            pop_sources=None,
             opt_method='SLSQP',
-            alpha=0.9,
-            iwnd=None):
+            alpha=1):
     """
     Optimise all admixtures in one window
-    :param iwnd: id of the window
+    :param popset_init: list of initial source populstions
     :param pop_names: names of initial populations
     :param pop_admix: names of mixed populations
     :param pop_sources: sources of admixtures
@@ -142,13 +145,11 @@ def opt_wnd(pop_names, popset_init,
     :return: -
     """
 
-    if (iwnd is None):
-        swnd = ''
-    else:
-        swnd = str(iwnd + 1)
 
 
     variables_estimated = dict()
+    var_mix_all = []
+
     for istep in range(len(admixture_steps)):
 
         idx_pop = [item for i in range(istep + 1) for item in admixture_steps[i]]
@@ -164,61 +165,67 @@ def opt_wnd(pop_names, popset_init,
             weight_sets, weights_used = define_used_weights(weight_sets, weights_used, variables)
             var_mix_all_init += [var_mix]
 
+        # matrix of distances from the root (covariance matrix)
         v_mx = get_cov(popset, variables)
+        # matrix of distances between leaves
         d_mx = get_dist(v_mx)
 
         # remove already estimated variables
         d_mx = d_mx.subs(variables_estimated)
-        var_mix_all = [[v.subs(variables_estimated) for v in var_mix]
+        # variances of the new populations
+        var_mix_all += [[v.subs(variables_estimated) for v in var_mix]
                        for var_mix in var_mix_all_init]
 
+        # variables which are not estimated
         variables_d = [v for v in variables if v not in variables_estimated.keys()]
+        # weights which are not estimated
         weight_sets = [[w for w in elem if w not in variables_estimated.keys()] for elem in weight_sets]
 
         # --------------------------
         # Optimisation
 
-
-        wnd_init = read_csv(path_data_in + pref_wnd_init + swnd + '.txt', sep='\t', index_col=0)
-
+        # get current names of populations
         pop_current = pop_names + [p for i, p in enumerate(pop_admix) if i in idx_pop]
-        wnd = wnd_init.loc[pop_current, pop_current]
+        dist_curr = dist_matrix.loc[pop_current, pop_current]
 
         # print(wnd)
         # print(d_mx)
 
-
-        param_val, variances = param_estim(d_mx, variables_d, weight_sets, var_mix_all, wnd,
+        param_val = param_estim(d_mx, variables_d,
+                                           weight_sets, dist_curr,
                                            opt_method=opt_method,
                                            alpha=alpha)
 
         param_val[param_val < 10 ** -6] = 0
 
-
         showl(list(zip(variables_d, param_val)))
-        showl(variances)
-
         variables_estimated.update(dict(zip(variables_d, param_val)))
 
-        # If you estimate the first tree without admixtures - do not estimate
-        # parameters around the root
-        if istep == 0 and len(admixture_steps[0]) == 0:
-            variables_estimated.pop(variables[0])
-            variables_estimated.pop(variables[1])
+    return variables_estimated
+        #
+        #
+
+        #
+        # # If you estimate the first tree without admixtures - do not estimate
+        # # parameters around the root
+        # if istep == 0 and len(admixture_steps[0]) == 0:
+        #     variables_estimated.pop(variables[0])
+        #     variables_estimated.pop(variables[1])
+
 
     # np.savetxt(X=list(variables_estimated.values()), fname=path_data_out + pref_out_res + str(iwnd + 1) + '.txt', delimiter='\t')
 
-    pop_all = pop_names + pop_admix
-    alpha_corresp = list(chain(*[[[pop_admix[i], pop_all[j]] for j in pop_sources[i]]
-                      for i in range(len(pop_sources))]))
-    alpha_extims = [[x, y] for x, y in variables_estimated.items()
-                    if is_weight(x)]
+    # pop_all = pop_names + pop_admix
+    # alpha_corresp = list(chain(*[[[pop_admix[i], pop_all[j]] for j in pop_sources[i]]
+    #                   for i in range(len(pop_sources))]))
+    # alpha_extims = [[x, y] for x, y in variables_estimated.items()
+    #                 if is_weight(x)]
 
-    f = open(path_data_out + pref_out_res + swnd + '.txt', 'w')
-    for corr, estim in list(zip(alpha_corresp, alpha_extims)):
-        # print(corr + estim)
-        f.write('{} {}: {}: {}\n'.format(corr[0], corr[1], estim[0], estim[1]))
-    f.close()
-
-
-    np.savetxt(X=variances, fname=path_data_out + pref_out_var + swnd + '.txt', delimiter='\t')
+    # f = open(path_data_out + pref_out_res + swnd + '.txt', 'w')
+    # for corr, estim in list(zip(alpha_corresp, alpha_extims)):
+    #     # print(corr + estim)
+    #     f.write('{} {}: {}: {}\n'.format(corr[0], corr[1], estim[0], estim[1]))
+    # f.close()
+    #
+    #
+    # np.savetxt(X=variances, fname=path_data_out + pref_out_var + swnd + '.txt', delimiter='\t')
---
+++ sim_testing.py
@@ -0,0 +1,173 @@
+"""
+Pipeline to optimise admixture graph of the known topology
+"""
+from ete3 import Tree
+
+import os
+from multiprocess import Pool
+from functools import partial
+from itertools import product
+
+from tree_cov import get_populations
+from migadmi_opt import *
+from utils import *
+from pandas import read_csv, DataFrame
+import sys
+
+
+# --------------------------
+# Files and paths
+path_data = 'simulation/sim_data/'
+# path_data_in = path_data + 'in2/'
+
+path_data_out = path_data + 'out_testing/'
+if not os.path.exists(path_data_out):
+    os.mkdir(path_data_out)
+
+n_sim = 100
+n_adm = 2
+pop_admix = ['admix' + str(i+1) for i in range(n_adm)]
+
+# for i_sim in range(1, n_sim+1):
+i_sim = 1
+def simulation(i_sim):
+    # Initial Tree
+    file_tree = path_data + 'sim_tree.nwk'
+    # file_dist = '{}s{}_dist.txt'.format(path_data_in, i_sim)
+    # file_admixture = '{}s{}_adm.txt'.format(path_data_in, i_sim)
+
+
+    # --------------------------
+    # Structure of the dataset
+    # Read tree
+    tree = Tree(file_tree)
+    pop_names = [node.name for node in tree.traverse("levelorder") if node.is_leaf()]
+    pop_names_all = pop_names + pop_admix
+    print(tree)
+    popset_init, variables_init = get_populations(tree, pop_names)
+    n_source = len(pop_names)
+
+    # Events
+    # Indexes of source populations
+    pop_sources = [list(np.sort(np.random.choice(n_source + i, np.random.choice(2)+2, replace=False)))
+     for i in range(n_adm)]
+
+    pop_sources_names = [[pop_names_all[i] for i in idxs]
+        for idxs in pop_sources]
+
+    # Admixture steps
+    admixture_steps = [list(range(n_adm))]
+    # Check possibility of admixtures
+    check_admixtures(pop_names, pop_sources, admixture_steps)
+
+    # --------------------------
+    # Get distance matrix
+    idx_pop = list(range(n_adm))
+    popset = popset_init
+    variables = variables_init
+    weight_sets = []
+    weights_used = []
+    var_mix_all_init = []
+
+    for i in idx_pop:
+        popset, variables, var_mix = create_admixture_alpha(popset,
+                                                            variables, pop_sources[i])
+        weight_sets, weights_used = define_used_weights(weight_sets, weights_used, variables)
+        var_mix_all_init += [var_mix]
+
+    # matrix of distances from the root (covariance matrix)
+    v_mx = get_cov(popset, variables)
+    # matrix of variances between leaves
+    d_mx = get_dist(v_mx)
+
+    # Create random values for parameters
+    variables_created = dict()
+    for v in variables:
+        if is_alpha(v) or is_branch(v):
+            variables_created[v] = np.random.uniform() * 0.9 + 0.1
+
+    for weights in weight_sets:
+        n_w = len(weights)
+        tmp = np.random.uniform(0, 1, n_w)
+        tmp /= sum(tmp)
+        for i, w in enumerate(weights):
+            variables_created[w] = tmp[i]
+
+    # Calculate distance matrix
+    variance_real = d_mx.subs(variables_created)
+    variance_real = DataFrame(np.array(variance_real), columns=pop_names_all,
+                   index=pop_names_all)
+    dist_real = variance_real ** (1/2)
+
+    # --------------------------
+    # Simulate frequencies
+    n_sim = 100
+    x_incr = dict()
+    for v in variables:
+        if not is_branch(v):
+            continue
+        x_incr[v] = np.random.normal(loc=0,
+                                     scale=variables_created[v] ** (1/2),
+                                     size=n_sim-1)
+
+    x = []
+    for perc_init in list(range(1, n_sim)):
+        f_init = perc_init / n_sim
+        x_init = np.log((1-f_init) / f_init)
+
+        x_increment = dict(variables_created)
+        for v in variables:
+            if not is_branch(v):
+                continue
+            x_increment[v] = x_incr[v][perc_init-1]
+
+        x_mx = np.array(v_mx.subs(x_increment)) + x_init
+        x_end = np.array(list(x_mx.diagonal()))
+
+        x += [x_end]
+
+    x_mx = np.array(x).transpose()
+
+    variance_sim = np.array([[np.array(x_mx[i] - x_mx[j], dtype=np.float64).std() ** 2
+                  for j in range(len(x_mx))]
+                 for i in range(len(x_mx))])
+
+    dist_sim = DataFrame(np.array(variance_sim ** (1/2)), columns=pop_names_all,
+                   index=pop_names_all)
+
+
+    # --------------------------------------------------
+    # Estimation
+    # admixture_steps = [[0], [1]]
+    popdisp_part = partial(popdisp, pop_names=pop_names,
+                           popset_init=popset_init,
+                           variables_init=variables_init,
+                           pop_admix=pop_admix,
+                           admixture_steps=admixture_steps,
+                           pop_sources=pop_sources)
+    # Estimation
+    variables_estim_real = popdisp_part(dist_matrix=dist_real)
+    variables_estim_sim = popdisp_part(dist_matrix=dist_sim)
+
+    delta_w_real = [np.mean([abs(variables_estim_real[w] - variables_created[w])
+                             for w in weights]) for weights in weight_sets]
+    delta_w_sim = [np.mean([abs(variables_estim_sim[w] - variables_created[w])
+                            for w in weights]) for weights in weight_sets]
+
+
+
+    with open('{}test{}.txt'.format(path_data_out, i_sim), 'w') as f:
+        sys.stdout = f
+        print(i_sim, delta_w_real, delta_w_sim)
+
+simulation(i_sim)
+
+
+n_sim = 100
+with Pool(n_sim) as workers:
+    pmap = workers.map
+    pmap(simulation, range(n_sim))
+
+
+
+
